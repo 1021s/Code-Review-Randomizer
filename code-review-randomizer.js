1cb086390240aae0bd714d38b71efb1d0df76398
Patrick Bennett <pbennettwa@gmail.com>
2019-11-27 12:11:20 -0800

    added functional code review generator

---
+++ code-review-randomizer.js
@@ -0,0 +1,21 @@
+// run this script after you do a pull request and group slack the individuals listed
+
+const codeReview = yourName => {
+  const seniorEngineers = ['Inna', 'Kyle', 'Matthew', 'Patrick', 'Sayer']
+  let peopleToReviewYourCode = [];
+
+  const randomizer = () => {
+    const engineerIndex = Math.floor(Math.random() * Math.floor(seniorEngineers.length));
+    if (seniorEngineers[engineerIndex] !== yourName) {
+      peopleToReviewYourCode.push(seniorEngineers[engineerIndex]);
+    }
+    if (peopleToReviewYourCode.length < 2) {
+      randomizer();
+    }
+  }
+
+  randomizer();
+  return peopleToReviewYourCode;
+}
+
+codeReview('yourName')
